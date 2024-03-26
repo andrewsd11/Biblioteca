d27c41c5c6a1bfdc32efdb46c640005a0bf1ef63
andrewsd11 <161460629+andrewsd11@users.noreply.github.com>
2024-03-26 08:21:57 -0300

    Add files via upload

--- config/urls.py
+++ config/urls.py
@@ -14,11 +14,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
+
 from django.contrib import admin
+from django.urls import path
+from django.views.generic import TemplateView
 from app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", IndexView.as_view(), name="Index")
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('livros.html', LivrosView.as_view(), name='livros'),
+    path('cidade.html', CidadesView.as_view(), name='cidade'),
+    path('autor.html', AutoresView.as_view(), name='autor'),
+    path('editora.html', EditorasView.as_view(), name='editora'),
+    path('leitor.html', LeitoresView.as_view(), name='leitor'),
+    path('genero.html', GenerosView.as_view(), name='genero'),
 ]
